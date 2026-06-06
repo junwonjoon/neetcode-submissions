f5cfd5d5ef456fda14951249599c96091a5cee03
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-06-06 16:25:55 -0400

    Add: permutation-string - submission-15

---
+++ Data Structures & Algorithms/permutation-string/submission-15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        l = 0 
+        r = len(s1)
+        s1_dict = {}
+        for letter in s1:
+            s1_dict[letter] = 1 + s1_dict.get(letter, 0)
+        while r <= len(s2):
+            test_dict = {}
+            for letter in s2[l:r]:
+                if s1_dict.get(letter, 0) == 0:
+                    break
+                test_dict[letter] = 1 + test_dict.get(letter, 0)
+            else:
+                if s1_dict == test_dict:
+                    return True
+            l += 1
+            r += 1
+        return False
+                
+
