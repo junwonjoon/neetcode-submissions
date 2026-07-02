8ec033c5aba1544d208bcc895e39ebe06d84d057
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-07-02 16:22:04 -0400

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.py
@@ -0,0 +1,13 @@
+from bisect import bisect_left
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_to_look = bisect_left([row[0] for row in matrix], target)
+        if row_to_look < len(matrix) and matrix[row_to_look][0] == target:
+            return True
+        else:
+            row_to_look -= 1
+        index = bisect_left(matrix[row_to_look], target)
+        if index < len(matrix[0]) and matrix[row_to_look][index] == target:
+            return True
+        else:
+            return False
