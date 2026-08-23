01a7d93828c182b126970647e099b20f131d77f1
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-08-23 18:47:53 -0400

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.py
@@ -0,0 +1,17 @@
+from bisect import bisect_left
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        while len(stones) >= 1:
+            heavier = stones.pop()
+            if not stones:
+                return heavier
+            lighter = stones.pop()
+            if lighter == heavier:
+                continue
+            else:
+                new_stone = heavier - lighter
+                stones.insert(bisect_left(stones, new_stone), new_stone)
+        return stones[0] if stones else 0
+
