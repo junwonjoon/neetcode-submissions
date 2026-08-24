d07cbf4078a05c197c986ca551398163da704e34
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-08-24 18:14:05 -0400

    Add: binary-tree-maximum-path-sum - submission-3

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-3.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        sol = -9999  
+        def local_max(root, prev):
+            nonlocal sol
+            if not root:
+                return
+            prev_val = -9999
+            if prev:
+                prev_val = prev.val
+            center = root.val
+            left = 0 
+            right = 0
+            if root.left:
+                left = root.left.val
+            if root.right:
+                right = root.right.val
+            root.val = max(center + left, center, center + right)
+            sol = max(sol, center, root.val, center + left + right)
+        queue = deque([root]) 
+        levels = [[(root, None)]]
+        while queue:
+            next_level = []
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                    next_level.append((curr.left, curr))
+                if curr.right:
+                    queue.append(curr.right)
+                    next_level.append((curr.right, curr))
+            levels.append(next_level)
+        levels.pop()
+        if len(levels) > 1:
+            while levels:
+                curr_level = levels.pop()
+                for root, parent in curr_level:
+                    local_max(root, parent)
+        else:
+            local_max(root, None)
+        return sol
