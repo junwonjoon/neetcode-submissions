64515b17498cb39f1c16a78613ba23a55d4fced2
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-09-16 22:24:57 -0400

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        total = 0
+        used = set()
+        total = 0
+        first_t = True 
+        while total not in used:
+            if first_t:
+                first_t = False
+                total = n
+            used.add(total)
+            new_total = 0
+            for num in [int(x) for x in str(total)]:
+                new_total += num ** 2
+            total = new_total
+            print(total)
+            if total == 1:
+                return True
+        return False
