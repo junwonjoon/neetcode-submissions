b102182309d967717687b651611e59755e042b7a
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-09-19 00:36:45 -0400

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        popped = nums.pop()
+        rob1_p, rob1_c, rob2_c, rob2_p = 0, 0, 0, 0
+        for n in nums:
+            temp = max(rob1_p + n, rob1_c)
+            rob1_p = rob1_c 
+            rob1_c = temp
+        nums.append(popped)
+        nums.pop(0)
+        for n in nums:
+            temp = max(rob2_p + n, rob2_c)
+            rob2_p = rob2_c 
+            rob2_c = temp
+        return max(rob2_c, rob1_c)
