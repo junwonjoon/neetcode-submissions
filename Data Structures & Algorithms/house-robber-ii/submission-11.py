2ae1aa3ad5458c1ead1ea567a5ad38948ba7b1f4
Wonjoon Jun <142065160+junwonjoon@users.noreply.github.com>
2026-09-19 00:40:46 -0400

    Add: house-robber-ii - submission-11

---
+++ Data Structures & Algorithms/house-robber-ii/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1_p, rob1_c, rob2_c, rob2_p = 0, 0, 0, 0
+        for n in nums[:-1]:
+            temp = max(rob1_p + n, rob1_c)
+            rob1_p = rob1_c 
+            rob1_c = temp
+        for n in nums[1:]:
+            temp = max(rob2_p + n, rob2_c)
+            rob2_p = rob2_c 
+            rob2_c = temp
+        return max(nums[0], rob2_c, rob1_c)
